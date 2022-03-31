d93e2098feeacca4019b47fa4f438e476616966f
gabrielmellooliveira <gabrielmello3011@gmail.com>
2022-03-30 21:47:36 -0300

    Configuração e utilização do novo repositório

--- aula-8-sqlite/src/index.js
+++ aula-8-sqlite/src/index.js
@@ -4,11 +4,31 @@ const CarroRepository = require('./repositories/CarroRepository')
 function main() {
   let repositorio = new CarroRepository()
 
-  let carro = new Carro('Cruze LT', 'Chevrolet', 2020, 'Cinza')
+  let carro = new Carro('Cruze LT', 'Chevrolet', 2021, 'Cinza')
+  let carro2 = new Carro('Cruze LTZ', 'Chevrolet', 2020, 'Branco')
+  let carro3 = new Carro('Cruze Premier', 'Chevrolet', 2021, 'Preto')
 
+  // Adicionar um carro
   repositorio.salvarCarro(carro)
+  repositorio.salvarCarro(carro2)
+  repositorio.salvarCarro(carro3)
 
-  console.log('Carros: ', repositorio.listarCarros())
+  // Listar carros
+  repositorio.listarCarros(function(carros) {
+    for (const carro of carros) {
+      // Remove um carro
+      repositorio.removerCarro(carro.getId())
+
+      // Editar um carro
+      carro.setModelo('SEM MODELO')
+      repositorio.editarCarro(carro)
+    }
+  })
+
+  // Filtrar carros por ano
+  repositorio.filtrarCarrosPorAno(2020, carros => {
+    console.log(carros)
+  })
 }
 
 main()
--- aula-8-sqlite/src/repositories/CarroRepository.js
+++ aula-8-sqlite/src/repositories/CarroRepository.js
@@ -1,36 +1,100 @@
+const { tratarErro } = require('../utils/logUtils')
+const { criarPastaSeNaoExistir } = require('../utils/pastaUtils')
+
 const Carro = require('../models/Carro')
+const sqlite = require('sqlite3')
+
+const CAMINHO_BANCO_DADOS = 'src/database/database.db'
+const CAMINHO_PASTA_BANCO_DADOS = './src/database'
 
 class CarroRepository {
   constructor() {
-    this._carros = []
+    criarPastaSeNaoExistir(CAMINHO_PASTA_BANCO_DADOS)
+
+    this._bancoDeDados = new sqlite.Database(CAMINHO_BANCO_DADOS, tratarErro)
   }
 
   salvarCarro(carro) {
     if (carro instanceof Carro) {
-      this._carros.push(carro)
+      this._bancoDeDados.run(
+        `INSERT INTO carros (carro_modelo, carro_marca, carro_ano, carro_cor) VALUES ($modelo, $marca, $ano, $cor)`, 
+        { 
+          $modelo: carro.getModelo(),
+          $marca: carro.getMarca(),
+          $ano: carro.getAno(),
+          $cor: carro.getCor()
+        }, 
+        tratarErro
+      )
     }
   }
 
-  listarCarros() {
-    return [...this._carros]
-  }
-
-  filtrarCarrosPorAno(ano) {
-    this._carros.filter(carro => carro.getAno() === ano)
-  }
-
-  removerCarro(modelo) {
-    this._carros = this._carros.filter(carro => carro.getModelo() !== modelo)
-  }
-
-  editarCarro(novoCarro) {
-    this._carros = this._carros.map(carro => {
-      if (carro.getModelo() === novoCarro.getModelo()) {
-        return novoCarro
-      } else {
-        return carro
+  listarCarros(callback) {
+    this._bancoDeDados.all(
+      'SELECT * FROM carros', 
+      function (err, rows) {
+        let carrosFormatados = rows.map(carro =>
+          new Carro(
+            carro.carro_modelo, 
+            carro.carro_marca, 
+            carro.carro_ano, 
+            carro.carro_cor,
+            carro.carro_id
+          )
+        )
+    
+        callback([...carrosFormatados])
       }
-    })
+    )
+  }
+
+  filtrarCarrosPorAno(ano, callback) {
+    this._bancoDeDados.all(
+      'SELECT * FROM carros WHERE carro_ano = $ano',
+      { $ano: ano },
+      function (err, rows) {
+        let carrosFormatados = rows.map(carro =>
+          new Carro(
+            carro.carro_modelo, 
+            carro.carro_marca, 
+            carro.carro_ano, 
+            carro.carro_cor,
+            carro.carro_id
+          )
+        )
+    
+        callback([...carrosFormatados])
+      }
+    )
+  }
+
+  editarCarro(carro) {
+    this._bancoDeDados.run(
+      `UPDATE carros SET 
+        carro_modelo = $modelo, 
+        carro_marca = $marca,  
+        carro_ano = $ano,  
+        carro_cor = $cor  
+      WHERE carro_id = $id`,
+      {
+        $id: carro.getId(),
+        $modelo: carro.getModelo(),
+        $marca: carro.getMarca(),
+        $ano: carro.getAno(),
+        $cor: carro.getCor()
+      },
+      tratarErro
+    )
+  }
+
+  removerCarro(idCarro) {
+    this._bancoDeDados.run(
+      'DELETE FROM carros WHERE carro_id = $id',
+      {
+        $id: idCarro
+      },
+      tratarErro
+    )
   }
 }
 
