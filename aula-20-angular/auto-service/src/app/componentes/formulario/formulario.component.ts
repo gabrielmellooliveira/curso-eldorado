80dff3a8dbc910739d057119c7f2005c6a49a71c
gabrielmellooliveira <gabrielmello3011@gmail.com>
2022-04-25 22:04:54 -0300

    Add FormControl

--- aula-20-angular/auto-service/src/app/componentes/formulario/formulario.component.ts
+++ aula-20-angular/auto-service/src/app/componentes/formulario/formulario.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { AuthGuardService } from 'src/app/guards/auth-guard.service';
 import { EldoradoService } from 'src/app/services/eldorado.service';
 
+import { FormControl, FormGroup, FormBuilder, FormArray, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.sass']
 })
 export class FormularioComponent implements OnInit {
-  login:string = '';
-  senha:string = '';
+  login: FormControl = new FormControl('', Validators.required);
+  senha: FormControl = new FormControl('', Validators.required);
+
+  mostrar: boolean = false;
 
   constructor(private eldoradoService: EldoradoService, private authGuard: AuthGuardService) { }
 
@@ -17,15 +21,21 @@ export class FormularioComponent implements OnInit {
     // this.listarCarros()
   }
 
-  logar(): void {
-    this.eldoradoService.autenticar(this.login, this.senha).subscribe(response => {
-      const token = response.content.token
+  mostrarTexto(): void {
+    this.mostrar = !this.mostrar;
+  }
 
-      if (token) {
-        this.authGuard.setAuth(true)
-        localStorage.setItem('token', token)
-      }
-    })
+  logar(): void {
+    if (this.login.status === 'VALID' && this.senha.status === 'VALID') {
+      this.eldoradoService.autenticar(this.login.value, this.senha.value).subscribe(response => {
+        const token = response.content.token
+
+        if (token) {
+          this.authGuard.setAuth(true)
+          localStorage.setItem('token', token)
+        }
+      })
+    }
   }
 
   listarCarros(): void {
